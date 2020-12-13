3729a8c776806b9127177b5d6fca9869e1889faa
Dallas Clarke <dallas.ag.clarke@gmail.com>
2020-12-13 15:07:14 -0800

    Todo.js and TodoInput.js skeleton

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 import './App.css';
+import {TodoInputContext, TodoContext} from "./context";
 
 function App() {
+  let initialTodoArray = window.localStorage.getItem("todos")
+  ? JSON.parse(window.localStorage.getItem("todos"))
+  : [];
+
+  const [todos, setTodos] = useState(initialTodoArray);
+
+
   return (
     <div className="App">
       
